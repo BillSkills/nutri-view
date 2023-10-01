3e38061d003af6ff2a159527f4804a150051ca73
Bill_Skills <billskills01@gmail.com>
2023-10-01 11:16:45 -0400

    deleted:    assets/NutriView.png 	modified:   main.py 	new file:   static/0.png 	modified:   templates/index.html 	deleted:    upload

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, url_for
+# IMPORTS
+from flask import Flask, render_template, request, url_for, session
 import function_olivier as fo
 import pandas as pd
 import openai
 import os
 from pathlib import Path
-
+import shutil
+from PIL import Image
 
 openai.api_key = "INSERT HERE"
 
@@ -14,27 +16,23 @@ app=Flask(__name__)
 UPLOAD_FOLDER = Path('static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+info=[[0,"banana", "tuesday 12 2023", "static/0.png",1,2,3,4,5]]
+info.reverse()
+button_clicked = 0
+
 @app.route('/', methods=["GET","POST"])
 def index():
     #info=fo.search_reviews(df, "porridge", n=1, pprint=True)
-    info=[[0,"banana", "wednesday 12 2023", 1,2,3,4,5],[1,"apple", "tuesday 13 2023", 1,2,3,4,5],[2,"pear", "thursday 12 2023", 1,2,3,4,5],[3,"strawberry", "friday 12 2023", 1,2,3,4,5]]
     selected_item=None
 
     if request.method=="POST":
-        button_clicked=request.form['button_clicked']
-        selected_item = info[int(button_clicked)]
+        button_clicked=int(request.form['button_clicked'])
+        selected_item = info[button_clicked]
         print(button_clicked)
         print(selected_item)
 
     if 'image' not in request.files:
-        print("I hate you")
-    else:
-        image = request.files['image']
-        print(image)
-        if image:
-            filename = app.config['UPLOAD_FOLDER'] / image.filename
-            image.save(filename)
-            img_url=url_for('static', filename=f'uploads/{image.filename}')
+        print("Image not in request.files")
 
     
     return render_template('index.html', info=info, selected_item=selected_item)
@@ -43,8 +41,28 @@ def index():
 def upload():
     if 'image' in request.files:
         image = request.files['image']
+        print(image)
+
         # Do something with the uploaded image, e.g., save it or process it
-        return 'Image uploaded successfully'
+        # return "uploaded"
+
+        if image:
+            # Ensure the 'static' folder exists
+            os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+            # Save the uploaded image to the 'static' folder
+            image.filename= str(button_clicked) + ".png"
+
+            # image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+            
+
+            filename = app.config['UPLOAD_FOLDER'] / image.filename
+            image.save(filename)
+
+            info.insert(0,[len(info),"grapes","saturday 12 2023",filename,1,2,3,4,5])
+            #img_url=url_for('static', filename=f'uploads/{image.filename}')
+
+        return render_template('index.html', info=info, selected_item=info[button_clicked])
     else:
         return 'No image file uploaded'
     
