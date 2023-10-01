9ca17fb0eef5fab59d1613337a0f7baacf626203
Bill_Skills <billskills01@gmail.com>
2023-09-30 23:30:08 -0400

    modified:   main.py 	modified:   templates/index.html

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ openai.api_key = "INSERT HERE"
 
 df = pd.read_csv('cleaned_data1.csv')
 app=Flask(__name__)
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
-    info=fo.search_reviews(df, "porridge", n=1, pprint=True)
+    #info=fo.search_reviews(df, "porridge", n=1, pprint=True)
+    info=[[0,"banana", "wednesday 12 2023", 1,2,3,4,5],[1,"apple", "tuesday 13 2023", 1,2,3,4,5],[2,"pear", "thursday 12 2023", 1,2,3,4,5],[3,"strawberry", "friday 12 2023", 1,2,3,4,5]]
+    selected_item=None
+
+    if request.method=="POST":
+        button_clicked=request.form['button_clicked']
+        selected_item = info[int(button_clicked)]
+        print(button_clicked)
+        print(selected_item)
     
-    return render_template('testfile.html', info=info)
+    return render_template('index.html', info=info, selected_item=selected_item)
 
 @app.route('/upload', methods=['POST'])
 def upload():
